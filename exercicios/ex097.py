ce3aa7bf9e3d4b5d12c64e82a778d0557bfe5f25
Guilherme Augusto Maas <guilherme.maas@gmail.com>
2019-09-17 12:55:57 -0300

    20190917

--- exercicios/ex097.py
+++ exercicios/ex097.py
@@ -5,3 +5,25 @@ print(f'{" " * 50}' + '{string}')
 print(f'{string:.<30}')
 """
 
+def imprimeLinha(quantidade):
+    print(f'=' * quantidade)
+
+def montaTitulo(titulo):
+    total_caracteres = len(titulo)
+    imprimeLinha(total_caracteres + 10)
+    print(f'{" " * 5}' + f'{titulo}')
+    imprimeLinha(total_caracteres + 10)
+
+
+while True:
+
+    texto = str(input('Digite um título qualquer: ')).upper().strip()
+    montaTitulo(texto)
+
+    continuar = ' '
+
+    while continuar not in 'NS':
+        continuar = str(input('Deseja exibir imprimir mais um título? S/N: \n')).upper().strip()
+        
+    if continuar == 'N':
+        break
