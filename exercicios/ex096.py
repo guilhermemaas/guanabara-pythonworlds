c61e8f51857bc87d60a21650f027117c82221283
Guilherme Augusto Maas <guilherme.maas@gmail.com>
2019-09-17 08:00:11 -0300

    20190917

--- exercicios/ex096.py
+++ exercicios/ex096.py
@@ -3,3 +3,12 @@ Faça um programa que tenha uma função chamada area(), que receba as deminsõe
 e mostre a área do terreno
 """
 
+def area(largura, comprimento):
+    return largura * comprimento
+
+largura = 5
+comprimento = 10
+
+print(f'Largura: {largura}m. Comprimento: {comprimento}. ', end='')
+print(f'Área: {area(largura, comprimento)}.')
+
--- exercicios/ex097.py
+++ exercicios/ex097.py
@@ -1,4 +1,7 @@
 """
 Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma
 mensagem com tamanho adaptável.
+print(f'{" " * 50}' + '{string}')
+print(f'{string:.<30}')
 """
+
